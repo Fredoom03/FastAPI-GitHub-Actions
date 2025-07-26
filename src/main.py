cde380f294257241c2179ef1cf8d89fd2516edbc
Fredoom03 <fredy_luque@hotmail.com>
2025-07-26 21:25:08 +0000

    Adding person model and method

---
+++ src/main.py
@@ -0,0 +1,20 @@
+from models.person_model import Person
+
+
+def create_person():
+    person = Person(
+        id=1,
+        first_name="Fredy",
+        last_name="Luque",
+        email="fredy_luque@hotmail.com",
+    )
+
+    return person
+
+
+def main():
+    print(create_person)
+
+
+if __name__ == "__main__":
+    main()
---
+++ src/test/test_person.py
@@ -0,0 +1,21 @@
+from src.models.person_model import Person
+
+
+person_mock = Person(
+    id=1,
+    first_name="Fredy",
+    last_name="Luque",
+    email="fredy_luque@hotmail.com",
+)
+
+
+class TestPerson:
+    def test_create_person(self):
+        person = Person(
+            id=1,
+            first_name="Fredy",
+            last_name="Luque",
+            email="fredy_luque@hotmail.com",
+        )
+
+        assert person is not None
